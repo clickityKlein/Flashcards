eaff4e4c2e94e3c7bb7805a8a09eb22f4ab7ae1a
Carl Klein <carlj.klein@gmail.com>
2023-01-12 20:34:14 -0800

    Started an examples file

---
+++ examples.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Examples file for flashcards.py module.
+
+1. Creating a Card object
+2. flip_card
+3. Creating a Deck object
+4. Loading a Deck (add_to_deck)
+    a. card
+    b. decks
+5. deck_to_df
+6. total_cards
+7. get_sets
+8. total_sets
+9. name_sets
+10. score_collection
+11. quiz
+12. single_set
+13. shuffle_collection
+14. delete_set
+15. reorder_collection
+16. delete_card
+17. reset_deck
+18. csv files
+    a. import_csv
+    b. export_csv
+"""
+
+import flashcards
+# 1. Creating a Card object
+card = Flashcard('question 1', 'answer 1', 'set a', 1)
+
+# 2. flip_card
+card.flip_card()
+"""
+Answer: answer 1
+Designate card as (p)ass, (f)ail, or (m)aybe:
+"""
+
+# 3. Creating a Deck object
+deck = Deck('deck 1')
+
+
+
+
+
+
